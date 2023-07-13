2ab6ebf4b3cd18a1adc5c02aa5cddd0ebb586f16
shanicenaliakanyongesa <shanicenyongesa@gail.com>
2023-07-13 02:31:03 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+                  print("Size of a char %d bytes(s)n",sizeof(char));
+                  print("Size of an int  %d bytes(s)n",sizeof(int));
+                  print("Size of a long int  %d bytes(s)n",sizeof( long int));
+                  print("Size of a long  long int  %d bytes(s)n",sizeof( long  long int));
+                  print("Size of a float  %d bytes(s)n",sizeof( float));
+
+  return (0)
+}
+
