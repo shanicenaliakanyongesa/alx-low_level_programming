6b3d0b1c9c67ee9843ccfa3dede3892edf3b1d5f
shanicenaliakanyongesa <shanicenyongesa@gail.com>
2023-08-24 13:38:11 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,20 @@
 section .data
-    hello: db 'Hello, Holberton', 10 ; 'Hello, Holberton!' plus a linefeed character
-    helloLen: equ $-hello ; Length of the 'Hello world!' string
+    greet db 'Hello, Holberton!', 0xa, 0
 
 section .text
-    global _start
+    global main
+
     extern printf
 
-_start:
-    push hello ; Push the address of hello onto the stack
-    call printf ; Call the printf function to print hello
-    add esp, 4 ; Adjust the stack pointer to remove the argument
-    mov eax, 1 ; The system call for exit (sys_exit)
-    xor ebx, ebx ; Exit with return "code" of 0 (no error)
-    int 80h
+main:
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [greet]
+    mov eax, 0
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
